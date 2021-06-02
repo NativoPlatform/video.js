b21fb14bd085c6f68ac3bc6683f6a7269ce8bca7
Tim Fischbach <info@timfischbach.de>
2021-06-02 21:51:20 +0200

    fix: allow Video.js to be required in an env without setTimeout (#7247)
    Some JavaScript environments don't implement setTimeout, which causes Video.js to fail on require. Instead, move our Dom.isReal() check earlier to before we call setTimeout.

--- src/js/setup.js
+++ src/js/setup.js
@@ -72,6 +72,11 @@ const autoSetup = function() {
  *        The videojs library function
  */
 function autoSetupTimeout(wait, vjs) {
+  // Protect against breakage in non-browser environments
+  if (!Dom.isReal()) {
+    return;
+  }
+
   if (vjs) {
     window['ntv-videojs'] = vjs;
   }
